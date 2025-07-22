4f19bc4448fd8c587ce5300743a4dfcf8de24975
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 21:31:46 +0530

    Create Finding_The_Average.asm

---
+++ Exp2_8051_Intermediate_Mathematical_Calculations/Finding_The_Average.asm
@@ -0,0 +1,19 @@
+;Finding the Average
+;In the data memory,load 10 bytes from 40H to 49H with some hexadecimal values
+
+org 0000h
+        mov r0,#40h    ;Pointer to the memory
+	      mov r2,#0ah    ;Average of 10 values is computed
+	      mov r3,#00h    ;R3 stores the number of values iterated through and is used later on in division
+	      mov a,#00h
+here:   mov b,@r0
+	      jnb b.0,label
+		    add a,b
+		    inc r3
+label : inc r0
+	      djnz r2,here
+	      mov b,r3
+	      div ab
+end
+
+;The Quotient and Remainder in hexadeciaml will be found in the Accumulator and B register respectively
