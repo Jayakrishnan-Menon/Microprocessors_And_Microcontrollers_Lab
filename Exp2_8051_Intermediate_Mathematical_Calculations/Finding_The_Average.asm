de103deeffd8429ba20c0be0e8a4cad9965f1d80
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 21:57:03 +0530

    Update Finding_The_Average.asm

--- Exp2_8051_Intermediate_Mathematical_Calculations/Finding_The_Average.asm
+++ Exp2_8051_Intermediate_Mathematical_Calculations/Finding_The_Average.asm
@@ -1,11 +1,11 @@
 ;Finding the Average
-;In the data memory,load 10 bytes from 40H to 49H with some hexadecimal values
+;In the data memory,load 10 bytes from 40H to 49H with some hexadecimal valuesoad the hex value from 40H in the data memory to the Accumulatoroad the hex value from 40H in the data memory to the Accumulator
 
 org 0000h
-        mov r0,#40h    ;R0 will be used as a Pointer to the data memory at 40H
-	mov r2,#0ah    ;Average of 10 values is computed
-	mov r3,#00h    ;R3 stores the number of values iterated through and is used later on in division
-	mov a,#00h
+        mov r0,#40h	;R0 will be used as a Pointer to the data memory at 40H
+	mov r2,#0ah	;Average of 10 values is computed
+	mov r3,#00h	;R3 stores the number of values iterated through and is used later on in division
+	mov a,#00h	
 here:   mov b,@r0
 	jnb b.0,label
 	add a,b
