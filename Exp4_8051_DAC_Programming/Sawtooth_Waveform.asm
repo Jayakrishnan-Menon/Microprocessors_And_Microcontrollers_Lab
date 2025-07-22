6433b12e7b5e1ed2241545edc9d167493e9eace9
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 23:42:18 +0530

    Update Sawtooth_Waveform.asm

--- Exp4_8051_DAC_Programming/Sawtooth_Waveform.asm
+++ Exp4_8051_DAC_Programming/Sawtooth_Waveform.asm
@@ -1,13 +1,13 @@
 org 0000h
 	
-again: mov a,#00h
-back:  mov p1,a
-	     inc a
+again:  mov a,#00h
+back:   mov p1,a
+        inc a
 	
-       cjne a,#0ffh,back
-back1: mov p1,a
-	    dec a
-	    cjne a,#00h,back1
-	    sjmp again
+        cjne a,#0ffh,back
+back1:  mov p1,a
+	dec a
+	cjne a,#00h,back1
+	sjmp again
 
 end
