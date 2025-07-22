6f35126c8ee112c2ce4d5d0bdfcd158c0ad76ed9
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 23:56:27 +0530

    Update Staircase_Waveform.asm

--- Exp4_8051_DAC_Programming/Staircase_Waveform.asm
+++ Exp4_8051_DAC_Programming/Staircase_Waveform.asm
@@ -1,16 +1,16 @@
 org 0000h
 
-again:  mov a,#00h          ;
-back:   mov p1,a            ;
-	acall delay         ;
-	add a,#64d          ;
-	cjne a,#0ffh,back   ;
-	sjmp again          ;
+again:  mov a,#00h          ;Initialize Accumulator
+back:   mov p1,a            ;Move the content of Accumulator to Port 1
+	acall delay         ;Call a delay to form the steps
+	add a,#64d          ;Increment the Accumulator in steps of #64D 
+	cjne a,#0ffh,back   ;Until the Accumulator reaches #0FFH, continue stepping
+	sjmp again          ;Run the program indefinitely
 
-delay:  mov r0,#0ffh        ;
+delay:  mov r0,#0ffh        ;Nested DJNZ Loops for implementing a delay
 h2:     mov r1,#0ah         ;
 h1:     djnz r1,h1          ;
 	djnz r0,h2          ;
-	ret                 ;
+	ret                 ;Call ends here and cpu returns to executing the main program
 
 end
