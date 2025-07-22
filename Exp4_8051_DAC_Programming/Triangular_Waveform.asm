bf02c3282a4469cc15d36316ce901b42ccea86f3
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 23:36:23 +0530

    Update Triangular_Waveform.asm

--- Exp4_8051_DAC_Programming/Triangular_Waveform.asm
+++ Exp4_8051_DAC_Programming/Triangular_Waveform.asm
@@ -1,10 +1,13 @@
 org 0000h
-	again: mov a,#00h
-	b1: mov p1,a
-	inc a
-	cjne a,#0ffh,b1
-	b2: mov p1,a
-	dec a
-	cjne a,#0ffh,b2
-	sjmp again
+
+again:  mov a,#00h       ;Initialize Accumulator
+b1:     mov p1,a         ;Move the content of Accumulator to Port 1
+	inc a            ;Increment the Accumulator
+	cjne a,#0ffh,b1  ;Compare the content of Accumulator to #0FFH, jump tp "b1" if the content is not #0FFH
+
+b2:     mov p1,a         ;Move the content of Accumulator to Port 1
+	dec a            ;Decrement the Accumulator
+	cjne a,#00h,b2   ;Compare the content of Accumulator to #00H, jump tp "b2" if the content is not #00H
+	sjmp again       ;Run the program indefinitely
+
 end
