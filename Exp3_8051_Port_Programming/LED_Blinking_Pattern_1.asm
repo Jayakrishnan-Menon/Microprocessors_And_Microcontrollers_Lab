053545ec0a6a75daa4700df6ee3f8ee561228142
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 23:05:27 +0530

    Update LED_Blinking_Pattern_1.asm

--- Exp3_8051_Port_Programming/LED_Blinking_Pattern_1.asm
+++ Exp3_8051_Port_Programming/LED_Blinking_Pattern_1.asm
@@ -14,15 +14,15 @@ org 0000h
 	      setb c        ;Set the carry
 	      mov a,#00h    ;Initialize the Accumulator
 
-again:  mov p1,a      ;Move the value in Accumulator to Port 1
+again:        mov p1,a      ;Move the value in Accumulator to Port 1
 	      rlc a         ;Shift and Rotate Accumulator contents to the left through the carry bit in the PSW
 	      acall delay   ;Call a delay to make the led blinking visible to the human eye
 	      sjmp again    ;Run the program indefinitely
 
-delay:  mov r0,#0ffh  ;Nested DJNZ Loops for implementing a delay
-l2:     mov r1,#0ffh  ;
-l1:     mov r2,#0ffh  ;
-here:   djnz r2,here  ;
+delay:        mov r0,#0ffh  ;Nested DJNZ Loops for implementing a delay
+l2:           mov r1,#0ffh  ;
+l1:           mov r2,#0ffh  ;
+here:         djnz r2,here  ;
 	      djnz r1,l1    ;
 	      djnz r0,l2    ;
 	      ret           ;Call ends here and cpu returns to executing the main program
