ab79725384498314dea992078d3cde7a1544f739
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-23 01:03:05 +0530

    Update Timer_Based_Waveform_Asymmetric.asm

--- Exp5_8051_Timer_Programming/Timer_Based_Waveform_Asymmetric.asm
+++ Exp5_8051_Timer_Programming/Timer_Based_Waveform_Asymmetric.asm
@@ -2,6 +2,7 @@
 org 0000h
 
         mov tmod,#10h   ;Declare the usage of Timer 1 in mode 1
+
 again:  mov th1,#0feh   ;Load #0FEH to the Higher Byte of Timer 1
 	mov tl1,#036h   ;Load #036H to the Lower Byte of Timer 1
 	setb p1.0       ;Set the state of the pin to high to generate the positive part of the waveform
@@ -9,6 +10,7 @@ again:  mov th1,#0feh   ;Load #0FEH to the Higher Byte of Timer 1
 here:   jnb tf1,here    ;Monitor the Flag for Timer 1 till it sets. Continue execution after it sets to 1
 	clr tf1         ;Clear the Timer Flag
 	clr tr1         ;Clear the Run Flag
+
 	mov th1,#0ffh   ;Load #0FFH to the Higher Byte of Timer 1
 	mov tl1,#0a4h   ;Load #0A4H to the Lower Byte of Timer 1
 	clr p1.0        ;Clear the state of the pin to low to generate the negative part of the waveform
