632152422617dba3312a1ada89f4c178fbe7df77
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-22 22:10:17 +0530

    Update Finding_The_Average.asm

--- Exp2_8051_Intermediate_Mathematical_Calculations/Finding_The_Average.asm
+++ Exp2_8051_Intermediate_Mathematical_Calculations/Finding_The_Average.asm
@@ -5,15 +5,14 @@ org 0000h
         mov r0,#40h	;R0 will be used as a Pointer to the data memory at 40H
 	mov r2,#0ah	;Average of 10 values is computed
 	mov r3,#00h	;R3 stores the number of values iterated through and is used later on in division
-	mov a,#00h	
-here:   mov b,@r0
-	jnb b.0,label
-	add a,b
-	inc r3
-label : inc r0
-	djnz r2,here
-	mov b,r3
-	div ab
+	mov a,#00h	;Initialize the Accumulator	
+here:   mov b,@r0	;Move the data being pointed at by R0 to the B register
+	add a,b		;Add a with b and store the result in a
+	inc r3		;Increment R3 value before the next iteration
+label : inc r0		;Increment the pointer to point to the next Byte
+	djnz r2,here 	;Till R1 becomes #00H continue looping and iterating through the data memory
+	mov b,r3	;Move the number of bytes to B register for the upcoming division operation
+	div ab		;Perform the Division operation
 end
 
 ;The Quotient and Remainder in hexadeciaml will be found in the Accumulator and B register respectively
