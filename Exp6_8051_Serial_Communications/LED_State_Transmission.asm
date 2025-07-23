a30393e90949473918fcf186b62b489fd46543c7
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-23 11:19:45 +0530

    Update LED_State_Transmission.asm

--- Exp6_8051_Serial_Communications/LED_State_Transmission.asm
+++ Exp6_8051_Serial_Communications/LED_State_Transmission.asm
@@ -12,7 +12,7 @@ again:	jb p1.0,LEDON         ;Monitor the State of the LED at Pin 0 of Port 1. G
 l2:     clr a                 ;Clear the Accumulator
 	movc a,@a+dptr        ;Move the Byte pointed to by the DPTR and offset by the value in the Accumulator to the Accumulator (Indexed Addressing)
 	jz exit1              ;Observe the Byte loaded in the Accumulator. If it is a String termination character "\0", then jump to "exit1". Else continue execution of the lext line
-	acall serial          ;Call the code snippet used for serial UART communication to Transmit the data
+	acall serial          ;Call the subroutine used for serial UART communication to Transmit the data
 	inc dptr              ;Point dptr ot the next location in the code memory
 	sjmp l2               ;Continue iterating through the message till the entire message is transmitted 
 exit1:  sjmp again            ;Once the String termination character is reached, monitor the LED again and continue execution indifinitely
@@ -21,12 +21,13 @@ LEDON:  mov dptr,#message1    ;Point the Data Pointer Register to the location o
 l1:     clr a                 ;Clear the Accumulator
 	movc a,@a+dptr        ;Move the Byte pointed to by the DPTR and offset by the value in the Accumulator to the Accumulator (Indexed Addressing)
 	jz exit2              ;Observe the Byte loaded in the Accumulator. If it is a String termination character "\0", then jump to "exit2". Else continue execution of the lext line
-	acall serial          ;Call the code snippet used for serial UART communication to Transmit the data
+	acall serial          ;Call the subroutine used for serial UART communication to Transmit the data
 	inc dptr              ;Point dptr ot the next location in the code memory
 	sjmp l1               ;Continue iterating through the message till the entire message is transmitted 
 exit2:  sjmp again            ;Once the String termination character is reached, monitor the LED again and continue execution indifinitely
 	
-serial: mov sbuf,a            ;Move the Character in the Accumulator to the sbuf register for transmitting
+serial:                       ;Subroutine for Serial Communication
+        mov sbuf,a            ;Move the Character in the Accumulator to the sbuf register for transmitting
 here:   jnb ti,here           ;Wait here till transmission is completed and the Transmission flag is set
 	clr ti                ;Clear the transmission flag to prepare before the next character is sent
 	ret                   ;Return to executing the main program
